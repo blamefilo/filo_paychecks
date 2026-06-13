8736bbb89aa599a847c53b31da2d9dc3e1ae92f4
filo <kloud.stu@gmail.com>
2026-06-14 02:21:30 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,33 +1,33 @@
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-name "filo_paychecks"
-author "filo studios."
-discord "https://discord.gg/EWKWXVBHK7"
-repository "https://github.com/blamefilo/filo_paychecks"
-description "Paychecks System"
-version "1.0.0"
+name 'filo_paychecks'
+author 'filo studios.'
+discord 'https://discord.gg/EWKWXVBHK7'
+repository 'https://github.com/blamefilo/filo_paychecks'
+description 'Paychecks System'
+version '1.0.0'
 
 shared_scripts {
-    "@ox_lib/init.lua",
-    "shared/sh-config.lua",
-    "shared/sh-init.lua",
+    '@ox_lib/init.lua',
+    'shared/sh-config.lua',
+    'shared/sh-init.lua',
 }
 
 server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "server/sv-*.lua"
+    '@oxmysql/lib/MySQL.lua',
+    'server/sv-*.lua'
 }
 
 client_scripts {
-    "client/cl-*.lua"
+    'client/cl-*.lua'
 }
 
 files {
-    "data/*",
+    'data/*',
 }
 
 dependencies {
-    "community_bridge"
+    'community_bridge'
 }
